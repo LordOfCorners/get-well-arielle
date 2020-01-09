84d845c877527e9c0a24692e9a3c7530a56a92b8
Michael Kahane <mkahane@hugeinc.com>
2020-01-08 23:40:23 -0500

    better css

--- src/components/CarouselContainer.vue
+++ src/components/CarouselContainer.vue
@@ -1,6 +1,11 @@
 <template>
-  <Carousel :autoplay="true" :autoplayTimeout="5000" :perPage="1">
-    <Slide :key="index" v-for="(row, index) in rows" class="slide">
+  <Carousel :autoplay="true" :autoplayTimeout="5000" :perPage="1" class="carousel">
+    <Slide
+      :key="index"
+      v-for="(row, index) in rows"
+      class="slide"
+      :class="[!row.picture ? 'only-message' : '' ]"
+    >
       <div>
         <img v-if="row.picture" :src="row.picture" />
         <div class="caption">
@@ -29,21 +34,32 @@ export default {
 
 <style>
 .slide {
-  align-items: center;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   max-width: 100%;
+  height: 100vh;
 }
 
+.only-message {
+  align-items: center;
+}
+.only-message p {
+  padding-top: 0;
+}
+
+.carousel {
+  height: 100vh;
+}
 img {
-  max-height: 400px;
+  max-height: 70vh;
   width: auto;
   max-width: 100%;
+  padding-top: 20px;
 }
 
 p {
-  max-width: 80vh;
+  max-width: 90vw;
   margin: 0 auto;
   padding-top: 15px;
 }
